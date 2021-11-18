deb86041ced1ff9fcaf0a692afce9b5849aa2d20
Bonu93 <luca.morucci.kb@gmail.com>
2021-11-18 12:10:36 +0100

    complete milestone 4

--- js/main.js
+++ js/main.js
@@ -18,6 +18,11 @@ Milestone 3
 Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
 
 Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+
+Milestone 4
+
+Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+
 */
 
 
@@ -31,6 +36,8 @@ const app = new Vue({
         activeContact: 0,
 
         userMessage: '',
+
+        searchQuery: '',
 
         dateNow: 'get date',
 
@@ -165,6 +172,18 @@ const app = new Vue({
         getDate() {
             const now = dayjs();
             this.dateNow = now.format('DD/MM/YYYY HH:mm:ss');
+        },
+
+        searchContact() {
+
+            this.contacts.forEach((contact) => {
+                if(contact.name.toLowerCase().includes(this.searchQuery.toLowerCase())) {
+                    contact.visible = true;
+                } else {
+                    contact.visible = false;
+                }
+                console.log(contact.visible);
+            })
         },
     },
 })
